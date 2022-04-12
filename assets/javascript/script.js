20dc57f9eebccc9dd0e2ec6e8ffd5da48de147a4
StuMartin22 <stuart.martin@rocketmail.com>
2022-04-11 21:31:22 -0500

    got my js file properly connected again...

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,55 +3,6 @@ var email = $('.emailBtn')
 var emailTest = $('#emailButtonId')
 var emailAddress = "stucodes22@gmail.com"
 var phoneNumber = "19188086027"
-var emailModal = $('#email')
-var titleLine = $('#emailModalLabel')
 var dropdownBtn = $('#emailDropdown')
 
-// email.click(testeroonie());
-// console.log(emailTest);
-// console.log(copyTest());
-// console.log(emailAddress);
-
-// emailTest.click(testeroonie());
-
-dropdownBtn.on("click", function (runTest) {
-    runTest();
-});
-
-
-function runTest () {
-    emailTest.click();
-console.log('emailbuttonID');
-
-email.click();
-console.log('email button class')
-
-emailModal.click();
-console.log('email Modal ID')
-
-titleLine.click();
-console.log('email title line');
-};
-// console.log(emailTest);
-// console.log(copyTest());
-// console.log(emailAddress);
-
-// phone.click(function(phoneCopy));
-
-// function copyTest(){
-//     navigator.clipboard.writeText('testing').then(function () {
-//         alert('It worked! Do a CTRL - V to paste')
-//     }, function () {
-//         alert('Failure to copy. Check permissions for clipboard')
-//     })
-// };
-// function testeroonie(){
-// navigator.clipboard.writeText(emailAddress)
-//         .then(() => {
-//         console.log("Text copied to clipboard...")
-//     })
-// };
-
-// email.click(testeroonie());
-// console.log(testeroonie());
-// console.log(email);
+alert ("test")
