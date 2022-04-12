7fd3c88dc9e02d1e3ce6d2dea8171eafc8e13cd3
StuMartin22 <stuart.martin@rocketmail.com>
2022-04-12 00:43:58 -0500

    tried unsuccessfully to implement a popup when button to copy email is pressed. BUT it does copy the email correctly so its a good start.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,9 +4,19 @@ var emailTest = $('#emailButtonId')
 var emailAddress = "stucodes22@gmail.com"
 var phoneNumber = "19188086027"
 var dropdownBtn = $('#emailDropdown')
+var popup = $('.popup')
 
-email.on('click', loggers)
+email.on('click', copyEmail)
 
 function loggers () {
     console.log('clickityClackity')
 }
+function copyEmail () {
+    navigator.clipboard.writeText(emailAddress);
+}
+
+// function popupEmail() {
+//     var popup = document.getElementById("myPopup");
+//     popup.classList.toggle("show");
+//   }
+
