a4a541d14870ed301a13955bf2959c0d35ccda58
Stuart (Stu) Martin <stucodes22@gmail.com>
2022-06-09 14:46:38 -0500

    added icons to footer

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Anchor, Button, Footer, Grommet, Header, Heading, Main, Menu, Paragraph, Text } from 'grommet';
-import { Home } from 'grommet-icons'
+import { Anchor, Button, Footer, Grommet, Header, Heading, Main, Menu, Nav, Paragraph, Text } from 'grommet';
+import { Download, Github, Home, Linkedin, Mail, Phone  } from 'grommet-icons'
 
 const theme = {
   global: {
@@ -15,18 +15,27 @@ const theme = {
 const App = () => {
   return (
     <Grommet theme={theme}>
+
 <Header background="brand">
   <Button icon={<Home />} hoverIndicator />
   <Menu label="account" items={[{ label: 'logout' }]} />
 </Header>
+
 <Main pad="large">
   <Heading></Heading>
   <Paragraph></Paragraph>
 </Main>
+
 <Footer background="brand" pad="medium">
-  <Text>Copyright</Text>
+  <Text>Contact me!</Text>
+  <Button icon={<Github />} hoverIndicator />
+  <Button icon={<Linkedin />} hoverIndicator />
+  <Button icon={<Phone />} hoverIndicator />
+  <Button icon={<Mail />} hoverIndicator />
+  <Button icon={<Download />} hoverIndicator />
   <Anchor label="About" />
 </Footer>
+
     </Grommet>
   );
 }
