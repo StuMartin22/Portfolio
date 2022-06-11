8113bac2d74413257e261c19f2d2693359ae1905
Stuart (Stu) Martin <stucodes22@gmail.com>
2022-06-11 17:49:41 -0500

    got phone and email cards looking better.

--- react-portfolio/src/pages/Contact.js
+++ react-portfolio/src/pages/Contact.js
@@ -1,5 +1,5 @@
 import { createElement, useState } from 'react';
-import { Box, Button, Card, CardBody, CardFooter, CardHeader, Carousel, Heading, Main, Paragraph, Tip, Notification} from 'grommet';
+import { Box, Button, Card, CardBody, CardFooter, CardHeader, Carousel, Heading, Main, Paragraph, Text, Tip} from 'grommet';
 import { Download, Github, Linkedin, Mail, Phone } from 'grommet-icons';
 
 const myemail = 'stucodes22@gmail.com'
@@ -12,6 +12,7 @@ const emailRunner = () => {
 
 const phoneRunner = () => {
     navigator.clipboard.writeText(myphone);
+    console.log('copied to clipboard')
 };
 
 
@@ -28,14 +29,31 @@ const Contact = () => {
                 </Paragraph>
             <Carousel controls='arrows'>
                 <Card justify='center' align='center' background='light-3' border={{'color':'dark-5', 'size':'medium'}}>
-                    <CardHeader pad="medium">Email</CardHeader>
-                        <CardBody pad="medium">Click the icon to copy my email address!</CardBody>
-                            <CardFooter background="light-2">
+                    <CardHeader justify='center' pad="small">
+                        <Text size='large'>Email</Text>
+                    </CardHeader>
+                        <CardBody alignContent='center' pad="medium">Click the icon to copy my email address!</CardBody>
+                            <CardFooter background='light-3'>
                                 <Tip content='Click here to copy!'>
                                     <Button align='center'
-                                    icon={<Mail color="accent-1" />}
+                                    icon={<Mail color="accent-1" size='large' />}
                                     hoverIndicator
-                                    onClick= {emailRunner}
+                                    onClick= { emailRunner }
+                                    />
+                                </Tip>
+                    </CardFooter>
+                    </Card>
+                    <Card justify='center' align='center' background='light-3' border={{'color':'dark-5', 'size':'medium'}}>
+                    <CardHeader justify='center' pad="small">
+                        <Text size='large'>Phone</Text>
+                    </CardHeader>
+                        <CardBody alignContent='center' pad="medium">Click the icon to copy my phone number!</CardBody>
+                            <CardFooter background='light-3'>
+                                <Tip content='Click here to copy!'>
+                                    <Button align='center'
+                                    icon={<Phone color="accent-1" size='large' />}
+                                    hoverIndicator
+                                    onClick= { phoneRunner }
                                     />
                                 </Tip>
                     </CardFooter>
