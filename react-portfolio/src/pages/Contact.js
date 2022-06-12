fc5322e60faed4115fac4f9f9175309341da5f1c
Stuart (Stu) Martin <stucodes22@gmail.com>
2022-06-12 12:36:52 -0500

    contact page wrapping up. Phone, email, linkedin, and hithub all work properly.

--- react-portfolio/src/pages/Contact.js
+++ react-portfolio/src/pages/Contact.js
@@ -1,6 +1,5 @@
-import { createElement, useState } from 'react';
 import { Box, Button, Card, CardBody, CardFooter, CardHeader, Carousel, Heading, Main, Paragraph, Text, Tip} from 'grommet';
-import { Download, Github, Linkedin, Mail, Phone } from 'grommet-icons';
+import { Github, Linkedin, Mail, Phone } from 'grommet-icons';
 
 const myemail = 'stucodes22@gmail.com'
 const myphone = '9188086027'
@@ -20,11 +19,11 @@ const Contact = () => {
   return(
 
     <Main>
-        <Box justify='center' align='center'>
+        <Box justifyContent='center' align='center'>
                 <Heading>
                     How to contact me!
                 </Heading>
-                <Paragraph>
+                <Paragraph margin={{bottom:'6vh'}}>
                     Are you liking what you see? Are you so offended that you just need to reach out to tell me? Then do so here with one of the following! Email is the best way to get a hold of me, however, any method will do!
                 </Paragraph>
             <Carousel controls='arrows'>
@@ -54,6 +53,34 @@ const Contact = () => {
                                     icon={<Phone color="accent-1" size='large' />}
                                     hoverIndicator
                                     onClick= { phoneRunner }
+                                    />
+                                </Tip>
+                    </CardFooter>
+                </Card>
+                <Card justify='center' align='center' background='light-3' border={{'color':'dark-5', 'size':'medium'}}>
+                    <CardHeader justify='center' pad="small">
+                        <Text size='large'>GitHub</Text>
+                    </CardHeader>
+                        <CardBody alignContent='center' pad="medium">Click the icon see my GitHub!</CardBody>
+                            <CardFooter background='light-3'>
+                                <Tip content='Click here to view my GitHub!'>
+                                    <Button align='center' href='https://www.github.com/StuMartin22' target='_blank' rel='noopener noreferrer'
+                                    icon={<Github color="accent-1" size='large' />}
+                                    hoverIndicator
+                                    />
+                                </Tip>
+                    </CardFooter>
+                </Card>
+                <Card justify='center' align='center' background='light-3' border={{'color':'dark-5', 'size':'medium'}}>
+                    <CardHeader justify='center' pad="small">
+                        <Text size='large'>LinkedIn</Text>
+                    </CardHeader>
+                        <CardBody alignContent='center' pad="medium">Click the icon to connect on LinkedIn!</CardBody>
+                            <CardFooter background='light-3'>
+                                <Tip content='Click here to view my LinkedIn!'>
+                                    <Button align='center' href='https://www.linkedin.com/in/stumartin22' target='_blank' rel='noopener noreferrer'
+                                    icon={<Linkedin color="accent-1" size='large' />}
+                                    hoverIndicator
                                     />
                                 </Tip>
                     </CardFooter>
