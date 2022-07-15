74818588aeb939ebf02e2f0bec281b20f21bcaef
Stuart (Stu) Martin <stucodes22@gmail.com>
2022-07-15 16:19:54 -0500

    launch

--- react-portfolio/src/constants/copyEmailFunction.js
+++
@@ -1,7 +0,0 @@
-const myemail = 'stucodes22@gmail.com'
-
-const emailRunner = () => {
-    navigator.clipboard.writeText(myemail);
-};
-
-export default emailRunner;
--- react-portfolio/src/constants/copyPhoneFunction.js
+++
@@ -1,7 +0,0 @@
-const myphone = '9188086027'
-
-const phoneRunner = () => {
-    navigator.clipboard.writeText(myphone);
-};
-
-export default phoneRunner;
