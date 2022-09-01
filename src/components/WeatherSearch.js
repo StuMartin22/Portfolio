b63e073a6f9ef1b9a1760dc666a1b391692ee8b0
Stuart (Stu) Martin <stucodes22@gmail.com>
2022-09-01 16:46:54 -0500

    added api fetch and options

--- src/components/WeatherSearch.js
+++ src/components/WeatherSearch.js
@@ -1,11 +1,15 @@
 import { AsyncPaginate } from "react-select-async-paginate"
+import { LocationAPI, LocationAPI_URL } from '../constants/api_fetch'
 
 const searchBar = ({onSearchChange}) => {
 
     const [search, setSearch] = useState(null);
 
     const loadOptions = (searchInput) => {
-        
+        return fetch(`${LocationAPI}/cities?limit=5&minPopulation=10000&namePrefix=${searchInput}`, LocationAPI_URL)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
     }
 
     const handleOnChange = (searchData) => {
