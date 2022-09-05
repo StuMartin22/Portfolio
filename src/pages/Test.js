42cd61c31292577de31ad2ce65caa0436b7406d0
Stuart (Stu) Martin <stucodes22@gmail.com>
2022-09-05 14:13:57 -0500

    better test page

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -2,7 +2,30 @@ import { Paragraph } from "grommet"
 
 const Mytest = () => {
     return (
-        <Paragraph>this is a test</Paragraph>
+        <Main pad="small" align='center' justify='center' flex='true' responsive='true' wrap='true' height={{ min: '100vh' }}>
+        <Page kind="narrow" align='center'>
+            <PageContent background="light-3" elevation='large' align='center' width={{ min: '178px', max: '80vw' }} border={{ size: 'medium', color: 'brand' }} round='small' margin={{ bottom: '3vh' }}>
+                {/* <Box justifyContent='center' align='center' flex={true} responsive={true}> */}
+                <Heading>
+                    test!
+                </Heading>
+                <Paragraph margin={{ bottom: '6vh' }}>
+                test </Paragraph>
+            </PageContent>
+            <Carousel controls='arrows' wrap='true' margin={{ bottom: '2vh' }} width={{ min: 'small' }} elevation='large'>
+                <ProjectCard
+                    HeaderText='test'
+                />
+                <ProjectCard
+                    HeaderText='test'
+                />
+                <ProjectCard
+                    HeaderText='test'
+                />
+            </Carousel>
+            {/* </Box> */}
+        </Page>
+    </Main>
     )}
 
     export default Mytest;
