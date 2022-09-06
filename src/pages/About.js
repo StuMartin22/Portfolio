d68705624b15a7680b18e5371145b2aa0135153f
Stuart (Stu) Martin <stucodes22@gmail.com>
2022-09-06 16:00:56 -0500

    got credly badge working on bio page. Added notation to bio page. Moved image from public to src

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,14 +1,19 @@
-import { Box, Heading, Main, Paragraph, Page, PageContent } from 'grommet'
+// importing styled components from Grommet
+import { Heading, Main, Paragraph, Page, PageContent } from 'grommet'
+// import Badge component
 import Badge from '../components/CredlyBadge';
 
+// function to return page info. 
 const Mybio = () => {
   return (
-    <Main pad="small" align='center' justify='center' flex='true' responsive='true' wrap='true' height={{min:'100vh'}}>
+    // Setting up content area of page
+    <Main pad="small" align='center' justify='center' flex='true' responsive='true' wrap='true' height={{ min: '100vh' }}>
       <Page kind="narrow" align='center'>
-        <PageContent background="light-3" elevation='large' align='center' width={{ min: 'auto', max: '80vw' }} margin={{bottom:'3vh'}} border={{ size: '3px', color: 'brand' }} round='small' justify='center' responsive={true}>
-          <Heading responsive={true}>My Bio</Heading>
+        <PageContent background="light-3" elevation='large' align='center' width={{ min: 'auto', max: '80vw' }} margin={{ bottom: '3vh' }} border={{ size: '3px', color: 'brand' }} round='small' justify='center' responsive='true'>
+          <Heading responsive='true'>My Bio</Heading>
         </PageContent>
         <PageContent background="light-3" elevation='large' align='center' width={{ min: 'auto', max: '80vw' }} overflow='auto' border={{ size: '3px', color: 'brand' }} round='small'>
+          {/* following is information about myself. Formatting included via props in Grommet tags */}
           <Paragraph fill='true' textAlign='center' justify='center'>
             I graduated from Rogers State University in 2017 with a Bachelor's of Science in Biology -  Environmental Conservation where I received the award of Best Senior Research Capstone for the program that year.
           </Paragraph>
@@ -24,11 +29,10 @@ const Mybio = () => {
           <Paragraph fill='true' textAlign='center' justify='center'>
             I am now graduated from my intensive full stack development bootcamp through the University of  Minnesota and excited to continue with this new venture of a lifelong passion of technology that satisfied the depth of my brain and it’s love of puzzles!
           </Paragraph>
-          {/* <Paragraph fill = 'true' textAlign='center' justify='center'> */}
-          {/* <Box height="small" width="small">
-            <Badge/>
-          </Box> */}
-          {/* </Paragraph> */}
+
+          {/* return credly badge imported from CredlyBadge component */}
+          <Badge />
+
           <Paragraph fill='true' textAlign='center' justify='center'>
             Languages and technologies I’ve worked with include:
             HTML, CSS, bootstrap (and other similar CSS frameworks), JavaScript, node.js, MySQL, Sequelize, NoSQL, MongoDB, Mongoose, Insomnia, GraphQL, and React!
@@ -39,5 +43,5 @@ const Mybio = () => {
   )
 };
 
-
+//exporting Mybio component for import elsewhere
 export default Mybio;
