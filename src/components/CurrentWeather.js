512723a5f47447e8ed4feef88d3c8dd00363929a
Stuart (Stu) Martin <stucodes22@gmail.com>
2022-09-12 00:06:28 -0500

    styling CurrentWeather component, changed min width on weather page, commented in CurrentWeatherComponent

--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -1,20 +1,33 @@
-// import { grommet, Paragraph } from "grommet";
-// import { } from grommet
+import { Card, CardHeader, CardBody, CardFooter, Paragraph, Text } from "grommet";
+import { Cloud } from "grommet-icons";
 
-// const CurrentWeather = () => {
-//     return (
-//         <Card width="medium" background="light-1">
-//             <CardHeader alignSelf='center' pad='medium' fill='horizontal' background='light-2' className="Date">Weather Outlook for Today: </CardHeader>
-//             <CardBody alignSelf='center'>
-//                 <Paragraph className="WeatherCurrentTemp">The Current Temperature is: 72 </Paragraph>
-//                 <Paragraph className="WeatherTypeIcon">Cloudy <Icons.Cloud color="blue" /></Paragraph>
-//             </CardBody>
-//             <CardFooter fill='horizontal' background="light-2">
-//                 <Paragraph className="WeatherHighTemp"> High: 79</Paragraph>
-//                 <Paragraph className="WeatherLowTemp"> Low: 69</Paragraph>
-//             </CardFooter>
-//         </Card>
-//     )
-// };
+const CurrentWeather = () => {
+    return (
+        <Card width={{ max:'60vw', min:'small' }} height={{min:'auto'}} wrap={true} justify='center' align='center' background='light-2' border={{'color':'brand', 'size':'medium'}}>
+            <CardHeader alignSelf='center' pad='medium' fill='horizontal' background='dark-2' className="Date">
+            <Text size='large' weight='bolder'>Weather Outlook for Today: </Text>
+            </CardHeader>
+            <CardBody alignSelf='center'>
+                <Paragraph>
+                    <Text weight='bold' margin={{right:'1vh'}}>The Current Temperature is:</Text> 
+                    <Text className="WeatherCurrentTemp" size='xx-large'>72°</Text>
+                </Paragraph>
+                <Paragraph alignSelf='center' className="WeatherTypeIcon">
+                    <Cloud color="brand" size='large' />
+                </Paragraph>
+            </CardBody>
+            <CardFooter background="light-2" padding={{right:'small', left:'small' }}>
+                <Paragraph> 
+                    <Text weight='bold' margin={{right:'1vh'}}>High:</Text>
+                    <Text className="WeatherHighTemp">75°</Text>
+                </Paragraph>
+                <Paragraph>
+                    <Text weight='bold' margin={{right:'1vh'}}> Low:</Text>
+                    <Text className="WeatherLowTemp">69°</Text>
+                </Paragraph>
+            </CardFooter>
+        </Card>
+    )
+};
 
-// export default CurrentWeather
+export default CurrentWeather;
--- src/pages/Weather.js
+++ src/pages/Weather.js
@@ -1,6 +1,6 @@
-// import CurrentWeather from "../components/currentWeather";
-import  SearchBar from "../components/WeatherSearch";
-import {Main, Page, PageContent, Heading, Paragraph } from "grommet"
+import CurrentWeather from "../components/CurrentWeather";
+import SearchBar from "../components/WeatherSearch";
+import { Main, Page, PageContent, Heading, Paragraph } from "grommet"
 
 const Weather = () => {
 
@@ -8,22 +8,22 @@ const Weather = () => {
         console.log(searchData)
     }
 
-    return(
-        <Main pad="small" align='center' justify='center' flex={true} responsive={true} wrap={true} height={{min:'100vh'}} >
-        <Page kind="narrow" align='center'>
-          <PageContent background="light-3" elevation='large' align='center' width={{min:'auto', max:'80vw'}} border={{size:'medium', color:'brand'}} round='small'>
-            <Heading>
-              Welcome to the weather!
-            </Heading>
-            <Paragraph>
-              This is a little app built using React, the CSS library grommet, OpenWeatherMapsAPI data, GeoDB Cities API data, and the Asyncpaginate node package.
-            </Paragraph>
-            <SearchBar onSearchChange={handleOnSearchChange} />
-            {/* <CurrentWeather /> */}
-          </PageContent>
-        </Page>
-      </Main>
-        
+    return (
+        <Main pad="small" align='center' justify='center' flex={true} responsive={true} wrap={true} height={{ min: '100vh' }} width={{ min: '190px' }} >
+            <Page kind="narrow" align='center'>
+                <PageContent background="light-3" elevation='large' align='center' width={{ min: '190px', max: '80vw' }} border={{ size: 'medium', color: 'brand' }} round='small' margin={{bottom:'5vh'}}>
+                    <Heading>
+                        Welcome to the weather!
+                    </Heading>
+                    <Paragraph>
+                        This is a little app built using React, the CSS library grommet, OpenWeatherMapsAPI data, GeoDB Cities API data, and the Asyncpaginate node package.
+                    </Paragraph>
+                    <SearchBar onSearchChange={handleOnSearchChange} />
+                </PageContent>
+                <CurrentWeather />
+            </Page>
+        </Main>
+
     )
 }
 
